552e387f5c6b9e218ec9f7355ac9d19ff24f7ee7
Ludmila Gubanova <darlingmila@yandex.ru>
2023-10-16 12:31:26 +0300

    add new functions and updated old functions

--- 15-task-object/app.js
+++ 15-task-object/app.js
@@ -4,59 +4,67 @@ const toDoList = {
   tasks: [
     {
       id: 1,
-      title: "Помыть посуду",
+      title: "Задача №1",
       priority: 1,
+    },
+    {
+      id: 2,
+      title: "Задача №2",
+      priority: 5,
+    },
+    {
+      id: 3,
+      title: "Задача №3",
+      priority: 3,
     },
   ],
 
-  addTask: function (toDoObj) {
+  getTaskById(id) {
+    return this.tasks.filter((task) => task.id === id)[0];
+  },
+
+  getTaskIndexById(id) {
+    return this.tasks.indexOf(this.getTaskById(id));
+  },
+
+  generateId() {
+    let newId = Math.floor(Math.random() * (999 - 0 + 1)) + 1;
+    if (this.getTaskById(newId)) return this.generateId();
+    return newId;
+  },
+
+  addTask(toDoObj) {
     const newToDo = {
-      id: this.tasks.length + 1,
+      id: this.generateId(),
       ...toDoObj,
     };
     this.tasks.push(newToDo);
   },
 
-  deleteTaskById: function (id) {
-    if (id > this.tasks.length) return;
-
-    this.tasks = this.tasks
-      .filter((toDo) => toDo.id !== id)
-      .map((toDo) => {
-        if (toDo.id < id) return toDo;
-
-        if (toDo.id > id) {
-          const newObj = {
-            ...toDo,
-            id: toDo.id - 1,
-          };
-          return newObj;
-        }
-      });
+  checkTaskExistenceById(id) {
+    const taskIndex = this.getTaskIndexById(id);
+    if (taskIndex === -1) return console.log(`Нет задачи с id ${id}`);
+    return true;
   },
 
-  modifyById: function (id, changeKey, newValue) {
-    if (id > this.tasks.length) return;
-
-    this.tasks = this.tasks
-      .map((toDo) => {
-        if (toDo.id !== id) return toDo;
-
-        for (let key in toDo) {
-          if (key === changeKey) {
-            toDo[key] = newValue;
-          }
-        }
-
-        return toDo;
-      });
+  deleteTaskById(id) {
+    if (!this.checkTaskExistenceById(id)) return;
+    this.tasks = this.tasks.filter((toDo) => toDo.id !== id);
   },
 
-  sortByPriority: function () {
+  updateTaskById(id, updatedTask) {
+    if (!this.checkTaskExistenceById(id)) return;
+    const taskIndex = this.getTaskIndexById(id);
+    this.tasks[taskIndex] = { ...this.tasks[taskIndex], ...updatedTask };
+  },
+
+  sortByPriority() {
     const copiedToDos = [...this.tasks];
     return copiedToDos.sort((a, b) => a.priority - b.priority);
   },
 };
+
+console.log(toDoList.getTaskById(1));
 
 toDoList.addTask({ title: "Постирать белье", priority: 3 });
 toDoList.addTask({ title: "Убрать комнату", priority: 5 });
@@ -66,13 +74,8 @@ toDoList.deleteTaskById(3);
 toDoList.deleteTaskById(1);
 toDoList.deleteTaskById(7);
 
-toDoList.addTask({ title: "Почистить ковер", priority: 4 });
-toDoList.addTask({ title: "Сходить в магазин", priority: 2 });
-
-toDoList.modifyById(1, "title", "Вкусно покушать");
-toDoList.modifyById(4, "priority", 3);
-toDoList.modifyById(8, "title", 1);
-toDoList.modifyById(1, "AAAAAAAAAAAAAAAAAA", 654767234);
+toDoList.updateTaskById(2, { title: "Смотреть в стену", priority: 2 });
+toDoList.updateTaskById(4, { title: "Задача №4", priority: 5 });
 
 console.log(toDoList.tasks);
 console.log(toDoList.sortByPriority());
